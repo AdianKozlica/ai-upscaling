d3dcbdd83f61d84eff17fc47709358f9d009c1dc
AdianKozlica <kozlicaadian@gmail.com>
2022-10-05 20:31:59 +0200

    Updated forms.py

--- ai_upscaling_proj/ai_upscaling_app/forms.py
+++ ai_upscaling_proj/ai_upscaling_app/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 class ImageForm(forms.Form):
     image = forms.ImageField()
-    scale = forms.IntegerField(initial=4)
+    scale = forms.IntegerField(
+            initial=4,
+            min_value=1,
+            max_value=10,
+            widget=forms.NumberInput(attrs={"type":"range"})
+        )
