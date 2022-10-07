2402a0a34faa98c8602fc9023a7d08878d4e8039
AdianKozlica <kozlicaadian@gmail.com>
2022-10-07 23:17:59 +0200

    Did collectstatic again and edited settings.py and urls.py

--- ai_upscaling_proj/static_root/js/buttons.js
+++
@@ -1,9 +0,0 @@
-const upscaleBtn = document.querySelector('.upscale-btn');
-
-upscaleBtn.addEventListener('click', () => {
-    upscaleBtn.style.boxShadow = '10px -10px 10px var(--bg-color-1)';
-});
-
-upscaleBtn.addEventListener('transitionend', () => {
-    upscaleBtn.style.boxShadow = '-10px 10px 10px var(--bg-color-1)';
-});
